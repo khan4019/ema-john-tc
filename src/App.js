6eff9eb217f6e04252d756812b6b742d8e8fcfff
That JS Dude <khan4019@gmail.com>
2017-08-19 15:40:24 -0500

    router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 
 import './App.css';
 
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
+import OrderReview from './components/OrderReview/OrderReview';
+import NotFound from './components/NotFound/NotFound';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header></Header>
-        <Shop></Shop>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Shop}></Route>
+            <Route path="/shop" component={Shop}></Route>
+            <Route path="/review" component={OrderReview}></Route>
+            <Route path="*" component={NotFound}></Route>
+          </Switch>
+        </BrowserRouter>
+        
       </div>
     );
   }
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NotFound extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Developer is sleeping</h2>
+            </div>
+        );
+    }
+}
+
+export default NotFound;
